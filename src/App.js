79773b2c6f2d8a61d1b952e417dc2867e37dfa97
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-07 22:26:54 +0200

    Navbar created and added in App.jsx

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './booking/Home';
 import Login from './auth/Login';
 import Register from './auth/Register';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route element={<Home />} path='/' />
         <Route element={<Login />} path='/login' />
