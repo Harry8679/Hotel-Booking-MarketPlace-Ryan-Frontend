ff046cac4b59759f85a81c43e72af18c01cc0c23
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-14 01:51:38 +0200

    Useless comments

--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -2,9 +2,6 @@ import React, { useState } from 'react';
 import Field from '../components/Field';
 
 const Register = () => {
-  // const [name, setName] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
 
   const [state, setState] = useState({
     name: '',
