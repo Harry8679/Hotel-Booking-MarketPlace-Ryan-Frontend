2a087a73743ac00d098a7af8df8983e7d1d81523
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-14 17:36:33 +0200

    Registration and redirection to login root

--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import Field from '../components/Field';
 import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
-
   const [state, setState] = useState({
     name: '',
     email: '',
     password: ''
   });
+
+  const navigate = useNavigate(); // Hook pour la redirection
 
   const handleSubmit = async (e) => {
     const { name, email, password } = state;
@@ -17,9 +21,22 @@ const Register = () => {
       console.table({ name, email, password });
       const res = await axios.post(`http://localhost:5500/api/v1/register`, { name, email, password });
 
+      setState({
+        name: '',
+        email: '',
+        password: ''
+      });
       console.log('REGISTER USER ===> ', res);
+      toast.success('User registered successfully. Please login.');
+
+      // Redirection après un délai pour afficher le toast
+      setTimeout(() => {
+        navigate('/login');
+      }, 6000); // 6 secondes de délai avant la redirection
+
     } catch (err) {
       console.log(err);
+      toast.error(err.response.data);
     }
   };
 
@@ -31,18 +48,15 @@ const Register = () => {
   }
 
   const registerForm = () => {
-    return <form onSubmit={handleSubmit} className='mt-3'>
-      <Field type_label='name' text_label='Your Name' type='text' id='name' name='name' placeholder='Enter name' value={state.name} handleChange={handleChange}  />
-      <Field type_label='email' text_label='Your Email' type='email' id='email' name='email' placeholder='Enter email' value={state.email} handleChange={handleChange}  />
-      <Field type_label='password' text_label='Your Password' type='password' id='password' name='password' placeholder='Enter password' value={state.password} handleChange={handleChange}  />
+    return (
+      <form onSubmit={handleSubmit} className='mt-3'>
+        <Field type_label='name' text_label='Your Name' type='text' id='name' name='name' placeholder='Enter name' value={state.name} handleChange={handleChange} />
+        <Field type_label='email' text_label='Your Email' type='email' id='email' name='email' placeholder='Enter email' value={state.email} handleChange={handleChange} />
+        <Field type_label='password' text_label='Your Password' type='password' id='password' name='password' placeholder='Enter password' value={state.password} handleChange={handleChange} />
 
-      {/* <div className="form-group mb-3">
-        <label htmlFor="name" className='form-label'>Your Name</label>
-        <input type="text" id='name' name='name' className='form-control' placeholder='Enter name' value={state.name} onChange={handleChange} />
-      </div> */}
-
-      <button className='btn btn-primary'>Registration</button>
-    </form>;
+        <button className='btn btn-primary'>Registration</button>
+      </form>
+    );
   };
 
   return (
@@ -50,6 +64,8 @@ const Register = () => {
       <div className='container-fluid bg-secondary p-5 text-center'>
         <h1>Register</h1>
       </div>
+
+      <ToastContainer />
 
       <div className="container">
         <div className="row">
@@ -59,7 +75,7 @@ const Register = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
